1d0ad08217a8d09e20243f91b86d156bff76116b
julienbigras <julien.bigras@hotmail.com>
2019-06-16 21:13:10 -0400

    responsive nav and other media queries

--- scripts.js
+++ scripts.js
@@ -5,3 +5,14 @@
 //     });
 
 // });
+
+const navSlide = () => {
+    const burger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.navLinks');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('navActive')
+    }) 
+}
+
+navSlide();
